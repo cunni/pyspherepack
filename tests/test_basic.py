48b9ab178c6cea838ebee69ea0e94c9c9d4e4222
cunni <jpcunni@gmail.com>
2018-08-17 10:19:01 -0400

    pytest fixture for better test perf

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,16 +2,28 @@ import pytest
 from pyspherepack import Box
 import numpy as np
 
-def test_Box_instance():
+# set up a module scoped box so that the test box (and really the pack()) is only instantiated once.
+@pytest.fixture(scope="module")
+def box_11packed():
+    b = Box(11,n_iters=50000)
+    b.pack()
+    return b
+
+def test_box_instance():
     # create Box
     b = Box(41) # 41 balls
     assert True
 
 def test_pack_two():
     # create Box
-    # make this a fixture so pack can run and be used for multiple tests
     b = Box(2,n_iters=10000)
     b.pack()
     assert np.isclose(b.ball_radius(),np.sqrt(2)/2,.01)
+
+def test_density(box_11packed):
+    assert box_11packed.density() > 60 # permissive, just to make sure
+
+def test_radius(box_11packed):
+    assert box_11packed.ball_radius() > 0.15 # permissive, just to make sure
 
 
