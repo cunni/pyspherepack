ba61e10af4de699805ffc935bfe9858fc88aa469
cunni <jpcunni@gmail.com>
2018-09-04 22:37:37 -0400

    manybox for random restarts, also plotting

--- pyspherepack/box.py
+++ pyspherepack/box.py
@@ -7,17 +7,18 @@ interactive(True)
 #import pylab
 import matplotlib.patches as patches
 from .utils import np_printoptions       
+import pickle
 
 class Box(object):
     """a fairly simple class, with some non-oop goofiness to play nicely with autograd grad and their adam"""
-    def __init__(self,n_balls,n_dims=None,n_iters=None,box=None,x=None):
+    def __init__(self,n_balls,n_dims=None,n_iters=None,box=None,logits=None):
         # number of balls in box
         self.n_balls = n_balls
         self.n_dims = n_dims if n_dims is not None else 2
         # defines the bounding box; just a vector of the outer corner; the inner corner is assumed to be the origin
         self.box = box if box is not None else np.array([1.0]*self.n_dims)
-        # defines the ball positions
-        self.x = x if x is not None else self.box*np.random.randn(self.n_balls,self.n_dims)
+        # defines the ball positions in logit space; always 
+        self.logits = logits if logits is not None else np.random.randn(self.n_balls,self.n_dims)
         # some optimization parameters
         self.n_iters = n_iters if n_iters is not None else 100000
         self.step_size = 0.001
@@ -26,59 +27,118 @@ class Box(object):
         """calculates the maximum size sphere that can be 
         packed in a given constellation of x points.  
         Note: assumes no boundary, so that's really for the warper to determine"""
-        x = x if x is not None else self.sig_box()  # still oop but allowing autograd to do its thing
+        x = x if x is not None else self.box_warp()  # still oop but allowing autograd to do its thing
         # Note that i is in the argument here because I was too lazy to rewrite autograd.misc.optimizers adam
         sum_squares = np.sum((x[np.newaxis,:,:] - x[:,np.newaxis,:])**2, axis=2) + np.diag([np.inf]*np.shape(x)[0])
         return 0.5*np.sqrt(np.min(sum_squares))
     
-    def sig_box(self,x=None):
-        """warps real x into the specified box.  Note here self.box is used in usual oop fashion,
-        though the fp approach of grad is such that we need grad to pass x around, hence not self.x"""
-        x = x if x is not None else self.x  # still oop but allowing autograd to do its thing
-        return (self.box / (1.0 + np.exp(-x)))
+    def box_warp(self,logits=None):
+        """warps real logits into the specified box.  Note here self.box is used in usual oop fashion,
+        though the fp approach of grad is such that we need grad to pass logits around, hence not self.logits"""
+        logits = logits if logits is not None else self.logits  # still oop but allowing autograd to do its thing
+        return (self.box / (1.0 + np.exp(-logits)))
     
     
-    def print_status(self,x=None,i=None,g=None):
+    def print_status(self,logits=None,i=None,g=None):
         """just a print callback"""
-        x = x if x is not None else self.x  # still oop but allowing autograd to do its thing
+        logits = logits if logits is not None else self.logits  # still oop but allowing autograd to do its thing
         if i % 5000 == 0:
-            print("{:9}|{:23}|{:20}".format(i, self.ball_radius(self.sig_box(x),i), self.density(x) ))
+            print("{:9}|{:23}|{:20}".format(i, self.ball_radius(self.box_warp(logits),i), self.density(logits) ))
     
     def pack(self):
         print("   Iter  |    Ball radius        |     Density  ")
-        self.x = adam(grad(lambda x,i: -1*self.ball_radius(self.sig_box(x),i)), self.x, num_iters=self.n_iters,callback=self.print_status)
+        self.logits = adam(grad(lambda logits,i: -1*self.ball_radius(self.box_warp(logits),i)), self.logits, num_iters=self.n_iters,callback=self.print_status)
         # one more print at final iteration
         self.print_status(i=self.n_iters)
         
-    def density(self,x=None):
-        x = x if x is not None else self.x  # still oop but allowing autograd to do its thing
-        rad = self.ball_radius(self.sig_box(x))
+    def density(self,logits=None):
+        logits = logits if logits is not None else self.logits  # still oop but allowing autograd to do its thing
+        rad = self.ball_radius(self.box_warp(logits))
         return 100*rad**2*np.pi*self.n_balls/(np.prod(self.box+(2*rad)))
     
-    def plot(self):
+    def plot(self,scaled_rad=None,clamp_edge=0.0):
         """visualize the balls packed in"""
-        xx = self.sig_box(self.x)
-        rad = self.ball_radius(xx)
+        x = self.box_warp(self.logits)
+        rad = self.ball_radius(x)
+        scaled_rad = scaled_rad if scaled_rad is not None else rad
+        scaled_box = scaled_rad/rad*(self.box+2*rad)
+        scaled_x = scaled_rad/rad*(x + rad)
         #rad_norm = rad/(1+2*rad) # not quite right, box is normalizing oddly when nonsquare
-        print("Optimized Ball radii: {:04.2f}".format(rad))
-        with np_printoptions(precision=3, suppress=True):
-                print('Ball centers:\n {}'.format(xx))
+        print("Optimized Ball radii: {:04.2f}, scaled {:04.2f}".format(rad,scaled_rad))
+        with np_printoptions(precision=6, suppress=True):
+                print('Ball centers (scaled):\n {}'.format(scaled_x))
         # print("Normalized (true) Ball radii: {:06.4f}".format(rad_norm))
         print("Density %: {:04.2f}%".format(self.density()))
         print("Waste %: {:04.2f}%".format(100-self.density()))
+        print("Density with clamp edge %: {:04.2f}%".format((self.density()*np.prod(scaled_box)/(scaled_box[1]*(scaled_box[0]+2*clamp_edge)))))
+        print("Waste with clamp edge %: {:04.2f}%".format(100-(self.density()*np.prod(scaled_box)/(scaled_box[1]*(scaled_box[0]+2*clamp_edge)))))
         if self.n_dims==2:
             fig,ax = plt.subplots()
-            # plot balls
-            for i in range(self.n_balls):
-                ax.add_artist(plt.Circle((xx[i,0],xx[i,1]),rad,fill=False,color='C0',linewidth=2))
-            # plot centers
-            plt.scatter(xx[:,0],xx[:,1])
             # plot bounding box
-            rect = patches.Rectangle((0,0)-rad,self.box[0]+2*rad,self.box[1]+2*rad,linewidth=2,edgecolor='k',facecolor='none')
+            #rect = patches.Rectangle((0,0)-rad,self.box[0]+2*rad,self.box[1]+2*rad,linewidth=2,edgecolor='k',facecolor='none')
+            rect = patches.Rectangle((-clamp_edge,0),scaled_box[0]+2*clamp_edge,scaled_box[1],hatch='x',linewidth=2,edgecolor='k',facecolor='none')
             # Add the patch to the Axes
             ax.add_patch(rect)
+            rect2 = patches.Rectangle((0,0),scaled_box[0],scaled_box[1],linewidth=2,edgecolor='k',facecolor='w')
+            ax.add_patch(rect2)
+            # plot balls
+            for i in range(self.n_balls):
+                ax.add_artist(plt.Circle((scaled_x[i,0],scaled_x[i,1]),scaled_rad,fill=False,color='C0',linewidth=2))
+            # plot centers
+            ax.add_artist(plt.scatter(scaled_x[:,0],scaled_x[:,1]))
             ax.axis('equal')
             ax.axes.get_xaxis().set_visible(False)
             ax.axes.get_yaxis().set_visible(False)
+            # add annotations
+            ax.add_artist(plt.scatter(0.0,0.0,color='k'))
+            ax.text(0.0, 0.0, '(0.00,0.00)',horizontalalignment='left',verticalalignment='top')
+            ax.add_artist(plt.scatter(scaled_box[0],0.0,color='k'))
+            ax.text(scaled_box[0], 0.0, '({:04.2f},0.00)'.format(scaled_box[0]),horizontalalignment='right',verticalalignment='top')
+            ax.add_artist(plt.scatter(0.0,scaled_box[1],color='k'))
+            ax.text(0.0, scaled_box[1], '(0.00,{:04.2f})'.format(scaled_box[1]),horizontalalignment='left',verticalalignment='bottom')
+            ax.add_artist(plt.scatter(scaled_box[0],scaled_box[1],color='k'))
+            ax.text(scaled_box[0], scaled_box[1], '({:04.2f},{:04.2f})'.format(scaled_box[0],scaled_box[1]),horizontalalignment='right',verticalalignment='bottom')
+            if clamp_edge > 0:
+                ax.add_artist(plt.scatter(-clamp_edge,0.0,color='k'))
+                ax.text(-clamp_edge, 0.0, '-{:03.1f}'.format(clamp_edge),horizontalalignment='left',verticalalignment='top')
+                ax.add_artist(plt.scatter(scaled_box[0]+clamp_edge,0.0,color='k'))
+                ax.text(scaled_box[0]+clamp_edge, 0.0, '+{:03.1f}'.format(clamp_edge),horizontalalignment='right',verticalalignment='top')
             plt.show() 
+
+class ManyBox(object):
+    """instantiates many boxes of a size, packs through them, for local optima silliness"""
+    def __init__(self,n_balls,n_boxes=None,locked=None,filename=None,**kwargs):
+        self.n_balls = n_balls
+        self.n_boxes = n_boxes if n_boxes is not None else 10
+        self.boxes = [Box(n_balls,**kwargs) for i in range(self.n_boxes)]
+        self.best_box = {'i':None, 'density':0, 'box':None}
+        self.locked = locked if locked is not None else True
+        self.filename = filename if filename is not None else 'data/manybox.pkl'
+
+    def pack(self):
+        for i in range(self.n_boxes):
+            print('=========packing box {}==========='.format(i))
+            self.boxes[i].pack()
+            self.best_box = self.best_box if self.best_box['density'] > self.boxes[i].density() else {'i':i,'density':self.boxes[i].density(),'box':self.boxes[i]}
+            self.save()
+            print('=========done box {}, with density {}========'.format(i,self.boxes[i].density()))
+
+    def density_distrib(self):
+        return [b.density() for b in self.boxes]
+
+    def save(self):
+        with open(self.filename,'wb') as f:
+            pickle.dump(self,f)
+
+    @classmethod
+    def load(cls,filename=None):
+        # https://stackoverflow.com/questions/2709800/how-to-pickle-yourself
+        filename = filename if filename is not None else 'data/manybox.pkl'
+        try: 
+            with open(filename,'rb') as f:
+                return pickle.load(f)
+        except:
+            raise IOError('unable to load')
+
+
 
